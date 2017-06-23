9e34f8b0c56cb066e0b3425e3e141e5535362680
Marius <marisolis.frankensteinas@gmail.com>
2017-06-23 19:02:32 +0300

    Improved VU meter code, tidied up the formatting

--- Arduino-vu/Arduino-vu.ino
+++ Arduino-vu/Arduino-vu.ino
@@ -1,39 +1,87 @@
 #include "FastLED.h"
 
+// Define the LED constants
 #define NUM_LEDS 60
 #define DATA_PIN 3
 #define CHIPSET WS2812
+#define BRIGHTNESS 255
 #define COLOR_ORDER GRB
 CRGB leds[NUM_LEDS];
 
 
 int k;
 
-void setup() { 
+void setup() {
+  // Init serial
   Serial.begin(115200);
   delay(3000);
+  // Init LEDs
   FastLED.addLeds<CHIPSET, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
-  // Initialize serial port 
+  FastLED.setBrightness(BRIGHTNESS);
 }
 void loop() {
   int n = Serial.available();
+  // If serial comm is available, read
   if (n > 0) {
     k = Serial.parseInt();
   }
-  if (k > 0 && k <= 30 ) 
+  // If data legit, light up the LEDs
+  if (k > 0 && k <= 30 ) {
     Serial.println(k);
-    showVU(k);
+    showVUtwo(k);
   }
 }
 
-void showVU(int k) {
+// Pattern from sides to center
+void showVUone(int k) {
+  int r = 0, g = 255, b = 0;
   for (int i = k; i < NUM_LEDS - 30; i++)
     leds[i] = CRGB::Black;
-  for (int i = 0; i < NUM_LEDS - 60 + k; i++)
-    leds[i] = CRGB::HotPink;
-  for (int i = NUM_LEDS - 1; i > NUM_LEDS - k; i--)
-    leds[i] = CRGB::HotPink;
-  for (int i = NUM_LEDS - k; i > NUM_LEDS - 30; i--)
+  for (int i = 0; i < k; i++) {
+    leds[i].setRGB(r, g, b);
+    r += 15;
+    g -= 25;
+    if (r > 255) r = 255;
+    if (g < 0) g = 0;
+  }
+  FastLED.show();
+  r = 0;
+  g = 255; 
+  for (int i = NUM_LEDS; i > NUM_LEDS - k - 1; i--) {
+    leds[i].setRGB(r, g, b);
+    r += 15;
+    g -= 25;
+    if (r > 255) r = 255;
+    if (g < 0) g = 0;
+  }
+  for (int i = NUM_LEDS - k - 1; i > NUM_LEDS - 30; i--)
+    leds[i] = CRGB::Black;
+  FastLED.show();
+}
+
+// Patern from center to sides
+void showVUtwo(int k) {
+  int r = 0, g = 255, b = 0;
+  for (int i = NUM_LEDS/2 - k; i >= 0; i--)
+    leds[i] = CRGB::Black;
+  for (int i = NUM_LEDS/2; i >= NUM_LEDS/2 - k - 1; i--) {
+    leds[i].setRGB(r, g, b);
+    r += 15;
+    g -= 25;
+    if (r > 255) r = 255;
+    if (g < 0) g = 0;
+  }
+  FastLED.show();
+  r = 0;
+  g = 255; 
+  for (int i = NUM_LEDS/2; i <= NUM_LEDS/2 + k + 1; i++) {
+    leds[i].setRGB(r, g, b);
+    r += 15;
+    g -= 25;
+    if (r > 255) r = 255;
+    if (g < 0) g = 0;
+  }
+  for (int i = NUM_LEDS/2 + k; i <= NUM_LEDS; i++)
     leds[i] = CRGB::Black;
   FastLED.show();
 }
--- Edison/simple_server_main.cpp
+++ Edison/simple_server_main.cpp
@@ -12,69 +12,51 @@ const int thread_num = 64;
 
 typedef mraa::Gpio* gpioarray;
 
+// Filter out bad data
 int getLeds(std::string data)
 {
-	unsigned char k;
-	if (data.length() <= 2 && data.length() > 0) {
-		k = std::stoi(data.c_str());
-		//std::cout << data << std::endl;
-	}
-	if (k >= 0 && k <= 10)
-		return k;
+  unsigned char k;
+  if (data.length() <= 2 && data.length() > 0) {
+    k = std::stoi(data.c_str());
+    //std::cout << data << std::endl;
 }
-
-void flashy_flashy(gpioarray gpio[ledAmount], int k)
-{
-  for (int i = k; i < ledAmount; i++)
-  {
-	  gpio[i]->write(0);
-	  //usleep(20000);
-  }
-  for (int i = 0; i < k; i++)
-  {
-	  gpio[i]->write(1);
-	  //usleep(20000);
-  }
+  if (k >= 0 && k <= 10)
+    return k;
 }
 
 int main ( int argc, char *argv[] )
 {
-  gpioarray gpio[ledAmount];
-  for (int i = 4; i < ledAmount + 4; i++)
-          gpio[i-4] = new mraa::Gpio(i);
-  for (int i = 0; i < ledAmount; i++)
-          gpio[i]->dir(mraa::DIR_OUT);
+  // Init the UART
+  mraa_uart_context uart;
+  uart = mraa_uart_init(0);
+  mraa_uart_set_baudrate(uart, 115200);
   double volume;
   std::thread t[thread_num];
   std::cout << "running....\n";
+  
+  // Create the server
+  try {
+    // Create the socket
+    ServerSocket server ( 30000 );
 
-  try
-    {
-      // Create the socket
-      ServerSocket server ( 30000 );
-
-      while ( true )
-        {
-
-          ServerSocket new_sock;
-          server.accept ( new_sock );
-
-          try
-            {
-              while ( true )
-                {
-                  std::string data;
-                  new_sock >> data;
-		  int leds = getLeds(data);
-		  if (leds >= 0 && leds <= 10) {
-		  	std::cout << leds << std::endl;
-		  	flashy_flashy(gpio, leds);
-		  }
-                }
+    while ( true )
+      {
+        ServerSocket new_sock;
+        server.accept ( new_sock );
+        try {
+            // Main loop
+            while ( true ) {
+              std::string data;
+              new_sock >> data;
+              data += "\n";
+              //strncpy(buf, data.c_str(), sizeof(buf));
+              const char *buf = data.c_str();
+              mraa_uart_write(uart, buf, strlen(buf));
             }
+        }
   catch ( SocketException& ) {}
-	}
-    }  
+    }
+  }  
   catch ( SocketException& e )
   {std::cout << "Exception was caught:" << e.description() << "\nExiting.\n";}
   return 0;
